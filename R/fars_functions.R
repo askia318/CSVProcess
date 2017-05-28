3337487605494aae444bf5ccd93a0d1b6779efb6
askia318 <ccli.mail@gmail.com>
2017-05-28 23:38:13 +0800

    change R file

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -114,8 +114,8 @@ fars_read_years <- function(years) {
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
         dplyr::bind_rows(dat_list) %>%
-                dplyr::group_by(year, MONTH) %>%
-                dplyr::summarize(n = n()) %>%
+                dplyr::group_by_(year, MONTH) %>%
+                dplyr::summarize_(n = n()) %>%
                 tidyr::spread(year, n)
 }
 
