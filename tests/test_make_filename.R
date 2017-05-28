43bffd37b451a379d2b2443eadc08b0a15347579
askia318 <ccli.mail@gmail.com>
2017-05-28 23:12:04 +0800

    change test file

--- tests/test_make_filename.R
+++ tests/test_make_filename.R
@@ -2,6 +2,6 @@ library(testthat)
 
 test_that("character make", {
     accident_year <- make_filename(2013)
-    expect_that(accident_year, is_identical_to("accident_2013.csv.bz2"))
+    expect_that(accident_year, is_identical_to("data/accident_2013.csv.bz2"))
     expect_that(accident_year, is_a("character"))
 })
