e3e5cc4e99fe26ad31e4cb689c2aad14f4130d8c
askia318 <ccli.mail@gmail.com>
2017-05-29 10:44:09 +0800

    Change R file and CSVProcess.Rmd

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -47,7 +47,7 @@ fars_read <- function(filename) {
 #' @export
 make_filename <- function(year) {
         year <- as.integer(year)
-        sprintf("data/accident_%d.csv.bz2", year)
+        sprintf("../data/accident_%d.csv.bz2", year)
 }
 
 #' Read CSV file and Filter data by Month and Year
